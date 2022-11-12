eeca3e02b10aac21388e66d81d75ed048f705ea8
roytcen <121845002+roytcen@users.noreply.github.com>
2022-11-12 13:27:22 +0000

    overflow fix

--- src/main/kotlin/com/xxmicloxx/noteBlockAPI/NBSDecoder.kt
+++ src/main/kotlin/com/xxmicloxx/noteBlockAPI/NBSDecoder.kt
@@ -71,4 +71,4 @@ object NBSDecoder {
         val byte1 = dis.readUnsignedByte()
         val byte2 = dis.readUnsignedByte()
         val byte3 = dis.readUnsignedByte()
-       
+        val byte4 = dis.readUnsignedByte()
