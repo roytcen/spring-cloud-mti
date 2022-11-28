386332570a829ea8d30a28fb24cf27bdabca221a
roytcen <121845002+roytcen@users.noreply.github.com>
2022-11-28 18:16:53 +0000

    Removed semicolons

--- src/main/kotlin/com/xxmicloxx/noteBlockAPI/Note.kt
+++ src/main/kotlin/com/xxmicloxx/noteBlockAPI/Note.kt
@@ -2,4 +2,6 @@ package com.xxmicloxx.noteBlockAPI
 
 import cn.nukkit.level.Sound
 
-class Note(instrument: Int, val key: Byte) 
+class Note(instrument: Int, val key: Byte) {
+    private var instrument = 0
+    val sou
