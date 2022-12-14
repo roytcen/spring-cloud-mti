3025ac5e27ead96480d55659969a37b70d42dc84
roytcen <121845002+roytcen@users.noreply.github.com>
2022-12-14 18:16:13 +0000

    1.16.1

--- src/main/kotlin/net/mcpes/hhm/nk/music/listener/PlayerListener.kt
+++ src/main/kotlin/net/mcpes/hhm/nk/music/listener/PlayerListener.kt
@@ -3,4 +3,5 @@ package net.mcpes.hhm.nk.music.listener
 import cn.nukkit.event.EventHandler
 import cn.nukkit.event.Listener
 import cn.nukkit.event.player.PlayerJoinEvent
-import cn.nukkit.event.pla
+import cn.nukkit.event.player.PlayerQuitEvent
+import net.mcpes.hhm.nk
